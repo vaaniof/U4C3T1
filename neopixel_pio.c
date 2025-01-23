ffcbcf94fedbba9963fc97fb66f4974e5f2d4b75
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-23 18:11:21 -0300

    Inclusão da Biblioteca pico_bootrom.h

--- neopixel_pio.c
+++ neopixel_pio.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "ws2818b.pio.h"
+#include "pico/bootrom.h"
 
 // Definição do número dos pinos.
 #define LED_COUNT 25
@@ -19,17 +21,16 @@
 #define C3 9
 #define C4 8
 
-//Matriz de Mapeamento
-char MatrizMapeamento [NUM_LINHAS][NUM_COLUNAS] = {
-    {'1','2','3','A'},
-    {'4','5','6','B'},
-    {'7','8','9','C'},
-    {'*','0','#','D'}
-};
+// Matriz de Mapeamento
+char MatrizMapeamento[NUM_LINHAS][NUM_COLUNAS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
 char ultimaTecla = 'X';
 
-//Protótipos das Funções
+// Protótipos das Funções
 void inicializar();
 char AcharDigito();
 void Processar(char);
@@ -50,27 +51,27 @@ void Animacao_7();
 void Animacao_8();
 void Animacao_9();
 
-
 // Definição de pixel GRB
-struct pixel_t {
+struct pixel_t
+{
   uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
 };
-typedef struct pixel_t pixel_t;
-typedef pixel_t npLED_t; // Mudança de nome de "struct pixel_t" para "npLED_t" por clareza.
+typedef struct pixel_t npLED_t;
 
-
-npLED_t leds[LED_COUNT]; //buffer de pixels que formam a matriz.
+npLED_t leds[LED_COUNT]; // buffer de pixels que formam a matriz.
 
 // Variáveis para uso da máquina PIO.
 PIO np_pio;
 uint sm;
 
-int main() {
+int main()
+{
   char tecla;
   stdio_init_all();
   inicializar();
 
-  while (true) {
+  while (true)
+  {
     tecla = AcharDigito();
     Processar(tecla);
     sleep_ms(1000);
@@ -78,92 +79,107 @@ int main() {
 }
 
 void inicializar(){
-    // inicialização dos pinos
-    npInit(LED_PIN);
-    npClear();
+  // inicialização dos pinos
+  npInit(LED_PIN);
+  npClear();
 
-    gpio_init(BUZZER_PIN);
-    gpio_set_dir(BUZZER_PIN, GPIO_OUT);
-    gpio_put(BUZZER_PIN,0);
+  gpio_init(BUZZER_PIN);
+  gpio_set_dir(BUZZER_PIN, GPIO_OUT);
+  gpio_put(BUZZER_PIN, 0);
 
-    gpio_init(R1);
-    gpio_set_dir(R1, GPIO_OUT);
-    gpio_put(R1, 1);  // Inicializando as linhas do teclado em nível alto
+  gpio_init(R1);
+  gpio_set_dir(R1, GPIO_OUT);
+  gpio_put(R1, 1); // Inicializando as linhas do teclado em nível alto
 
-    gpio_init(R2);
-    gpio_set_dir(R2, GPIO_OUT);
+  gpio_init(R2);
+  gpio_set_dir(R2, GPIO_OUT);
+  gpio_put(R2, 1);
+
+  gpio_init(R3);
+  gpio_set_dir(R3, GPIO_OUT);
+  gpio_put(R3, 1);
+
+  gpio_init(R4);
+  gpio_set_dir(R4, GPIO_OUT);
+  gpio_put(R4, 1);
+
+  // Inicializando os pinos das colunas como entrada com pull up
+  gpio_init(C1);
+  gpio_set_dir(C1, GPIO_IN);
+  gpio_pull_up(C1);
+
+  gpio_init(C2);
+  gpio_set_dir(C2, GPIO_IN);
+  gpio_pull_up(C2);
+
+  gpio_init(C3);
+  gpio_set_dir(C3, GPIO_IN);
+  gpio_pull_up(C3);
+
+  gpio_init(C4);
+  gpio_set_dir(C4, GPIO_IN);
+  gpio_pull_up(C4);
+}
+char AcharDigito(){
+  for (int linha = 0; linha < 4; linha++)
+  {
+    // Coloca todas as linhas em nível alto
+    gpio_put(R1, 1);
     gpio_put(R2, 1);
-
-    gpio_init(R3);
-    gpio_set_dir(R3, GPIO_OUT);
     gpio_put(R3, 1);
-
-    gpio_init(R4);
-    gpio_set_dir(R4, GPIO_OUT);
     gpio_put(R4, 1);
 
-    // Inicializando os pinos das colunas como entrada com pull up
-    gpio_init(C1);
-    gpio_set_dir(C1, GPIO_IN);
-    gpio_pull_up(C1);
-
-    gpio_init(C2);
-    gpio_set_dir(C2, GPIO_IN);
-    gpio_pull_up(C2);
-
-    gpio_init(C3);
-    gpio_set_dir(C3, GPIO_IN);
-    gpio_pull_up(C3);
-
-    gpio_init(C4);
-    gpio_set_dir(C4, GPIO_IN);
-    gpio_pull_up(C4);
-}
-char AcharDigito() {
-    for (int linha = 0; linha < 4; linha++) {
-        // Coloca todas as linhas em nível alto
-        gpio_put(R1, 1);
-        gpio_put(R2, 1);
-        gpio_put(R3, 1);
-        gpio_put(R4, 1);
-
-        // Coloca a linha atual em nível baixo
-        switch (linha) {
-            case 0: gpio_put(R1, 0); break;
-            case 1: gpio_put(R2, 0); break;
-            case 2: gpio_put(R3, 0); break;
-            case 3: gpio_put(R4, 0); break;
-        }
-
-        // Verifica as colunas
-        if (!gpio_get(C1)) return MatrizMapeamento[linha][0];
-        if (!gpio_get(C2)) return MatrizMapeamento[linha][1];
-        if (!gpio_get(C3)) return MatrizMapeamento[linha][2];
-        if (!gpio_get(C4)) return MatrizMapeamento[linha][3];
+    // Coloca a linha atual em nível baixo
+    switch (linha)
+    {
+    case 0:
+      gpio_put(R1, 0);
+      break;
+    case 1:
+      gpio_put(R2, 0);
+      break;
+    case 2:
+      gpio_put(R3, 0);
+      break;
+    case 3:
+      gpio_put(R4, 0);
+      break;
     }
 
-    // Retorna um caractere nulo caso nenhuma tecla tenha sido pressionada
-    return '\0';
+    // Verifica as colunas
+    if (!gpio_get(C1))
+      return MatrizMapeamento[linha][0];
+    if (!gpio_get(C2))
+      return MatrizMapeamento[linha][1];
+    if (!gpio_get(C3))
+      return MatrizMapeamento[linha][2];
+    if (!gpio_get(C4))
+      return MatrizMapeamento[linha][3];
+  }
+
+  // Retorna um caractere nulo caso nenhuma tecla tenha sido pressionada
+  return '\0';
 }
 void Processar(char tecla){
-    
-    if (tecla != '\0'){
 
-        //escreve a tecla somente se for diferente da ultima pressionada, evitando leituras repetidas 
-        if (ultimaTecla != tecla)
-            printf("Tecla %c Pressionada\n",tecla);
-        ultimaTecla = tecla;
-        ExecutaAcao(tecla);
-        }
-        sleep_ms(100);
+  if (tecla != '\0')
+  {
+
+    // escreve a tecla somente se for diferente da ultima pressionada, evitando leituras repetidas
+    if (ultimaTecla != tecla)
+      printf("Tecla %c Pressionada\n", tecla);
+    ultimaTecla = tecla;
+    ExecutaAcao(tecla);
+  }
+  sleep_ms(100);
 }
-void ModoBootsel() {
-    printf("Entrando em modo BOOTSEL...\n");
-    sleep_ms(1000);  // Aguarde um segundo antes de reiniciar
-    rom_reset_usb_boot(0,0);  // Reinicia e entra no modo BOOTSEL
+void ModoBootsel(){
+  printf("Entrando em modo BOOTSEL...\n");
+  sleep_ms(1000);           // Aguarde um segundo antes de reiniciar
+  reset_usb_boot(0,0); // Reinicia e entra no modo BOOTSEL
 }
-//Inicializa a máquina PIO para controle da matriz de LEDs.
-void npInit(uint pin) {
+void npInit(uint pin){
+  // Inicializa a máquina PIO para controle da matriz de LEDs.
 
   // Cria programa PIO.
   uint offset = pio_add_program(pio0, &ws2818b_program);
@@ -171,7 +187,8 @@ void npInit(uint pin) {
 
   // Toma posse de uma máquina PIO.
   sm = pio_claim_unused_sm(np_pio, false);
-  if (sm < 0) {
+  if (sm < 0)
+  {
     np_pio = pio1;
     sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
   }
@@ -180,96 +197,110 @@ void npInit(uint pin) {
   ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
 
   // Limpa buffer de pixels.
-  for (uint i = 0; i < LED_COUNT; ++i) {
+  for (uint i = 0; i < LED_COUNT; ++i)
+  {
     leds[i].R = 0;
     leds[i].G = 0;
     leds[i].B = 0;
   }
 }
+void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b){
+  // Atribui uma cor RGB a um LED.
 
-//Atribui uma cor RGB a um LED.
-void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
   leds[index].R = r;
   leds[index].G = g;
   leds[index].B = b;
 }
+void npClear(){
+  // Limpa o buffer de pixels.
 
-//Limpa o buffer de pixels.
-void npClear() {
   for (uint i = 0; i < LED_COUNT; ++i)
     npSetLED(i, 0, 0, 0);
 }
+void npWrite(){
+  // Escreve os dados do buffer nos LEDs.
 
-//Escreve os dados do buffer nos LEDs.
-void npWrite() {
   // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
-  for (uint i = 0; i < LED_COUNT; ++i) {
+  for (uint i = 0; i < LED_COUNT; ++i)
+  {
     pio_sm_put_blocking(np_pio, sm, leds[i].G);
     pio_sm_put_blocking(np_pio, sm, leds[i].R);
     pio_sm_put_blocking(np_pio, sm, leds[i].B);
   }
   sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
 }
+void ExecutaAcao(char tecla)
+{
+  // executa as ações de cada Tecla
 
-void ExecutaAcao(char tecla){
-        //executa as ações de cada Tecla
-        switch (tecla){
-        case '0' :
-          Animacao_0();
-          break;
-        case '1' :
-          Animacao_1();
-          break;
-        case '2' :
-          Animacao_2();
-          break;
-        case '3' :
-          Animacao_3();
-          break;
-        case '4' :
-          Animacao_4();
-          break;
-        case '5' :
-          Animacao_5();
-          break;
-        case '6' :
-          Animacao_6();
-          break;
-        case '7' :
-          Animacao_7();
-          break;
-        case '8' :
-          Animacao_8();
-          break;
-        case '9' :
-          Animacao_9();
-          break;
-        case 'A':
-          npClear();
-          npWrite();
-          break;
-        case 'B':
-          for (uint i = 0; i < LED_COUNT; ++i)
-            npSetLED(i, 0, 0, 255);
-          break;
-        case 'C':
-          for (uint i = 0; i < LED_COUNT; ++i)
-            npSetLED(i, 204, 0, 0);
-          break;
-        case 'D':
-          for (uint i = 0; i < LED_COUNT; ++i)
-            npSetLED(i, 0, 127, 0);
-          break;
-        case '#':
-          for (uint i = 0; i < LED_COUNT; ++i)
-            npSetLED(i, 50, 50, 50);
-          break;
-        case '*':
-          ModoBootsel();
-          break;
-        default:
-            printf("[ERRO] VALOR NAO ESPERADO RECEBIDO");
-            exit(1);
-            break;
-            }
+  switch (tecla)
+  {
+  case '0':
+    Animacao_0();
+    break;
+  case '1':
+    Animacao_1();
+    break;
+  case '2':
+    Animacao_2();
+    break;
+  case '3':
+    Animacao_3();
+    break;
+  case '4':
+    Animacao_4();
+    break;
+  case '5':
+    Animacao_5();
+    break;
+  case '6':
+    Animacao_6();
+    break;
+  case '7':
+    Animacao_7();
+    break;
+  case '8':
+    Animacao_8();
+    break;
+  case '9':
+    Animacao_9();
+    break;
+  case 'A':
+    npClear();
+    npWrite();
+    break;
+  case 'B':
+    for (uint i = 0; i < LED_COUNT; ++i)
+      npSetLED(i, 0, 0, 255);
+    break;
+  case 'C':
+    for (uint i = 0; i < LED_COUNT; ++i)
+      npSetLED(i, 204, 0, 0);
+    break;
+  case 'D':
+    for (uint i = 0; i < LED_COUNT; ++i)
+      npSetLED(i, 0, 127, 0);
+    break;
+  case '#':
+    for (uint i = 0; i < LED_COUNT; ++i)
+      npSetLED(i, 50, 50, 50);
+    break;
+  case '*':
+    ModoBootsel();
+    break;
+  default:
+    printf("[ERRO] VALOR NAO ESPERADO RECEBIDO");
+    exit(1);
+    break;
+  }
 }
+void Animacao_0(){}
+void Animacao_1(){}
+void Animacao_2(){}
+void Animacao_3(){}
+void Animacao_4(){}
+void Animacao_5(){}
+void Animacao_6(){}
+void Animacao_7(){}
+void Animacao_8(){}
+void Animacao_9(){}
