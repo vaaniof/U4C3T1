3654ef2aac932192705a12a6fa06bc07e3c6eae1
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-23 17:03:55 -0300

    Preparação para uso da matriz de leds

---
+++ neopixel_pio.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2818b.pio.h"
+
+// Definição do número dos pinos.
+#define LED_COUNT 25
+#define LED_PIN 7
+#define NUM_LINHAS 4
+#define NUM_COLUNAS 4
+#define BUZZER_PIN 21
+#define R1 18
+#define R2 16
+#define R3 19
+#define R4 17
+#define C1 20
+#define C2 4
+#define C3 9
+#define C4 8
+
+// Definição de pixel GRB
+struct pixel_t {
+  uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
+};
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Mudança de nome de "struct pixel_t" para "npLED_t" por clareza.
+
+
+npLED_t leds[LED_COUNT]; //buffer de pixels que formam a matriz.
+
+// Variáveis para uso da máquina PIO.
+PIO np_pio;
+uint sm;
+
+//Inicializa a máquina PIO para controle da matriz de LEDs.
+void npInit(uint pin) {
+
+  // Cria programa PIO.
+  uint offset = pio_add_program(pio0, &ws2818b_program);
+  np_pio = pio0;
+
+  // Toma posse de uma máquina PIO.
+  sm = pio_claim_unused_sm(np_pio, false);
+  if (sm < 0) {
+    np_pio = pio1;
+    sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
+  }
+
+  // Inicia programa na máquina PIO obtida.
+  ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+  // Limpa buffer de pixels.
+  for (uint i = 0; i < LED_COUNT; ++i) {
+    leds[i].R = 0;
+    leds[i].G = 0;
+    leds[i].B = 0;
+  }
+}
+
+//Atribui uma cor RGB a um LED.
+void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
+  leds[index].R = r;
+  leds[index].G = g;
+  leds[index].B = b;
+}
+
+//Limpa o buffer de pixels.
+void npClear() {
+  for (uint i = 0; i < LED_COUNT; ++i)
+    npSetLED(i, 0, 0, 0);
+}
+
+//Escreve os dados do buffer nos LEDs.
+void npWrite() {
+  // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
+  for (uint i = 0; i < LED_COUNT; ++i) {
+    pio_sm_put_blocking(np_pio, sm, leds[i].G);
+    pio_sm_put_blocking(np_pio, sm, leds[i].R);
+    pio_sm_put_blocking(np_pio, sm, leds[i].B);
+  }
+  sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
+}
+
+int main() {
+
+  // Inicializa entradas e saídas.
+  stdio_init_all();
+
+  // Inicializa matriz de LEDs NeoPixel.
+  npInit(LED_PIN);
+  npClear();
+
+  // Aqui, você desenha nos LEDs.
+
+  npWrite(); // Escreve os dados nos LEDs.
+
+  // Não faz mais nada. Loop infinito.
+  while (true) {
+    sleep_ms(1000);
+  }
+}
